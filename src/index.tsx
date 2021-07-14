7ed91b36ddc5693b50efb3633b4e0361e718ba82
Gabriel Moraru <gabrielcmoraru@github.com>
2021-07-14 20:14:10 +0100

    fix repeated code submition execution

--- src/index.tsx
+++ src/index.tsx
@@ -26,6 +26,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
