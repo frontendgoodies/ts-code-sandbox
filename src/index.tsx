a1a8851be76b6131ea61da709b293df1794c9de0
Gabriel Moraru <gabrielcmoraru@github.com>
2021-07-14 20:05:11 +0100

    render output to iframe(very basic)

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -36,8 +38,22 @@ const App = () => {
     });
     console.log(result);
 
-    setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+        window.addEventListener('message', (event) => {
+          eval(event.data)
+        }, false)
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -49,6 +65,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} />
     </div>
   );
 };
