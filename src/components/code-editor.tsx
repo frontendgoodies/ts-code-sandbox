dde945ef9cff98440496f4366976de30530697b2
Gabriel Moraru <gabrielcmoraru@github.com>
2021-07-14 20:58:06 +0100

    add monaco code editor + minimal config

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,24 @@
+import MonacoEditor from '@monaco-editor/react';
+
+const CodeEditor = () => {
+  return (
+    <MonacoEditor
+      language='javascript'
+      theme='dark'
+      height='500px'
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        fontLigatures: true,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
+};
+
+export default CodeEditor;
