a19d3dc162c07b8ba245b82bb7a3c2c4b8f27bb0
Gabriel Moraru <gabrielcmoraru@gmail.com>
2021-07-17 11:32:02 +0100

    refactor - create preview component

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
   const startService = async () => {
@@ -27,8 +28,6 @@ const App = () => {
       return;
     }
 
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -39,30 +38,9 @@ const App = () => {
         global: 'window',
       },
     });
-    console.log(result);
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-        window.addEventListener('message', (event) => {
-          try {
-            eval(event.data)
-          } catch (err) {
-            const root = document.querySelector('#root')
-            root.innerHTML = '<div style="color:red"><h4>Runtime error:</h4>' + err + '</div>'
-            console.error(err);
-          }
-        }, false)
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
@@ -70,19 +48,10 @@ const App = () => {
         initialValue={"const test = 'a'"}
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        title='preview'
-        ref={iframe}
-        sandbox='allow-scripts'
-        srcDoc={html}
-      />
+      <Preview code={code} />
     </div>
   );
 };
