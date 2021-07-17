bf3998eb30c70cb184e6cdc0870d7923104f8be7
LAPTOP-73KHUE3T\bobyh <gabrielcmoraru@gmail.com>
2021-07-17 13:35:58 +0100

    integrate horizontal resizing

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -17,10 +17,12 @@ const CodeCell = () => {
   return (
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-        <CodeEditor
-          initialValue={"const test = 'a'"}
-          onChange={(value) => setInput(value)}
-        />
+        <Resizable direction='horizontal'>
+          <CodeEditor
+            initialValue={"const test = 'a'"}
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
 
         <Preview code={code} />
       </div>
--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,22 +1,33 @@
 import './resizable.css';
-import { ResizableBox } from 'react-resizable';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox
-      minConstraints={[Infinity, 100]}
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      width={Infinity}
-      height={300}
-      resizeHandles={['s']}
-    >
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      width: window.innerWidth * 0.75,
+      height: Infinity,
+      resizeHandles: ['e'],
+    };
+  } else {
+    resizableProps = {
+      minConstraints: [Infinity, 100],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      width: Infinity,
+      height: 300,
+      resizeHandles: ['s'],
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
