20e12fdd1494a5fedccab51c7f1815e151e9cae0
LAPTOP-73KHUE3T\bobyh <gabrielcmoraru@gmail.com>
2021-07-18 19:05:21 +0100

    add action bar component to update cell position or delete

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,6 +1,8 @@
+import './cell-list-item.css';
 import { Cell } from '../state';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
+import ActionBar from './action-bar';
 
 interface CellListItemsProps {
   cell: Cell;
@@ -9,12 +11,20 @@ interface CellListItemsProps {
 const CellListItem: React.FC<CellListItemsProps> = ({ cell }) => {
   let child: JSX.Element =
     cell.type === 'code' ? (
-      <CodeCell cell={cell} />
+      <>
+        <div className='action-bar-wrapper'>
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
     ) : (
-      <TextEditor cell={cell} />
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
     );
 
-  return <div>{child}</div>;
+  return <div className='cell-list-item'>{child}</div>;
 };
 
 export default CellListItem;
