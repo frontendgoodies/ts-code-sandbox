abb37dafdcab48a8dfaff1cf932e4c140b999333
Gabriel Moraru <gabrielcmoraru@gmail.com>
2021-07-19 17:57:08 +0100

    add bundle redux reducer, action and action types

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -20,54 +20,57 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = produce((state: CellsState = initialState, action: Action) => {
-  switch (action.type) {
-    case ActionType.UPDATE_CELL:
-      const { id, content } = action.payload;
-      state.data[id].content = content;
-      return state;
-
-    case ActionType.DELETE_CELL:
-      delete state.data[action.payload];
-      state.order = state.order.filter((id) => id !== action.payload);
-      return state;
-
-    case ActionType.MOVE_CELL:
-      const { direction } = action.payload;
-      const index = state.order.findIndex((id) => id === action.payload.id);
-      const targetIndex = direction === 'up' ? index - 1 : index + 1;
-
-      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+const reducer = produce(
+  (state: CellsState = initialState, action: Action): CellsState => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload;
+        state.data[id].content = content;
         return state;
-      }
 
-      state.order[index] = state.order[targetIndex];
-      state.order[targetIndex] = action.payload.id;
-      return state;
+      case ActionType.DELETE_CELL:
+        delete state.data[action.payload];
+        state.order = state.order.filter((id) => id !== action.payload);
+        return state;
 
-    case ActionType.INSERT_CELL_AFTER:
-      const cell: Cell = {
-        content: '',
-        type: action.payload.type,
-        id: nanoid(),
-      };
+      case ActionType.MOVE_CELL:
+        const { direction } = action.payload;
+        const index = state.order.findIndex((id) => id === action.payload.id);
+        const targetIndex = direction === 'up' ? index - 1 : index + 1;
 
-      state.data[cell.id] = cell;
+        if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+          return state;
+        }
 
-      const foundIndex = state.order.findIndex(
-        (id) => id === action.payload.id
-      );
+        state.order[index] = state.order[targetIndex];
+        state.order[targetIndex] = action.payload.id;
+        return state;
 
-      if (foundIndex < 0) {
-        state.order.unshift(cell.id);
-      } else {
-        state.order.splice(foundIndex + 1, 0, cell.id);
-      }
+      case ActionType.INSERT_CELL_AFTER:
+        const cell: Cell = {
+          content: '',
+          type: action.payload.type,
+          id: nanoid(),
+        };
 
-      return state;
-    default:
-      return state;
-  }
-}, initialState);
+        state.data[cell.id] = cell;
+
+        const foundIndex = state.order.findIndex(
+          (id) => id === action.payload.id
+        );
+
+        if (foundIndex < 0) {
+          state.order.unshift(cell.id);
+        } else {
+          state.order.splice(foundIndex + 1, 0, cell.id);
+        }
+
+        return state;
+      default:
+        return state;
+    }
+  },
+  initialState
+);
 
 export default reducer;
