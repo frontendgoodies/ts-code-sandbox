00c1bc8ef15bb10a23a279d8a74eef49fe6c5537
Gabriel Moraru <gabrielcmoraru@gmail.com>
2021-07-20 20:41:33 +0100

    add internal show function that outputs string, objects, jsx

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -17,7 +17,24 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
 
-    const cumulativeCode = [];
+    const cumulativeCode = [
+      `
+        import _React from 'react';
+        import _ReactDOM from 'react-dom';
+        const root = document.querySelector('#root');
+        const show = (value) => {
+          if (typeof value === 'object') {
+            if (value.$$typeof && value.props ) {
+              _ReactDOM.render(value, root )
+            } else {
+              root.innerHTML = JSON.stringify(value)
+            }
+          } else {
+            root.innerHTML = value
+          }
+        }
+      `,
+    ];
     for (let c of orderedCells) {
       if (c.type === 'code') {
         cumulativeCode.push(c.content);
