a2980997c424416f52a1b5f89d6ef782b12171ad
LAPTOP-73KHUE3T\bobyh <gabrielcmoraru@gmail.com>
2021-07-21 21:04:46 +0100

    add a crude cli command tool that will pass info to the local api express server

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function () {
-    console.log('Server is running');
-});
+exports.serve = void 0;
+var serve = function (port, filename, dir) {
+    console.log("Traffic on port: " + port);
+    console.log("saving/fetching cells from " + filename);
+    console.log("file is in directory " + dir);
+};
+exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log('Server is running');
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log(`Traffic on port: ${port}`);
+  console.log(`saving/fetching cells from ${filename}`);
+  console.log(`file is in directory ${dir}`);
 };
