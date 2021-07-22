2aac48db217850e6b5dc97f5dfa147ce281593e8
Gabriel Moraru <gabrielcmoraru@gmail.com>
2021-07-22 20:43:26 +0100

    add expres for local api package and plug it into the cli

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,9 @@
+import express from 'express';
+
 export const serve = (port: number, filename: string, dir: string) => {
-  console.log(`Traffic on port: ${port}`);
-  console.log(`saving/fetching cells from ${filename}`);
-  console.log(`file is in directory ${dir}`);
+  const app = express();
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
+  });
 };
